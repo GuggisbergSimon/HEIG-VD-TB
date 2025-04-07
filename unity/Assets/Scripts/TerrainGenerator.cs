3b01d3510666d185442593297de7748386ba65d1
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2025-04-07 16:40:24 +0200

    feat: bigger play area : 8x8 km
    feat: terrain populator with buildings

--- unity/Assets/Scripts/TerrainGenerator.cs
+++ unity/Assets/Scripts/TerrainGenerator.cs
@@ -3,13 +3,10 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class TerrainGenerator : MonoBehaviour {
-    [Header("Terrain")] [SerializeField] private int depth = 20;
+    [SerializeField] private int depth = 20;
     [SerializeField] private int size = 256;
     [SerializeField] private float scale = 20f;
     [SerializeField] private Material terrainMaterial;
-    [Header("Buildings")] [SerializeField] private GameObject[] buildingPrefabs;
-    [SerializeField] private float yOffset = -5f;
-    [SerializeField] private int buildingCount = 100;
 
     private void OnValidate() {
         GenerateTerrain();
@@ -34,43 +31,5 @@ public class TerrainGenerator : MonoBehaviour {
             terrainData.SetHeights(0, 0, heights);
             terrain.materialTemplate = terrainMaterial;
         }
-    }
-
-    [ContextMenu("Place Buildings")]
-    private void PlaceBuildings() {
-        foreach (var terrain in Terrain.activeTerrains) {
-            TerrainData terrainData = terrain.terrainData;
-            for (int i = terrain.transform.childCount - 1; i >= 0; i--) {
-                DestroyImmediate(terrain.transform.GetChild(i).gameObject);
-            }
-
-            int posX = (int)terrain.transform.position.x;
-            int posZ = (int)terrain.transform.position.z;
-
-            for (int i = 0; i < buildingCount; i++) {
-                Vector3 position = new Vector3(
-                    Random.Range(posX, posX + size),
-                    0,
-                    Random.Range(posZ, posZ + size)
-                );
-
-                position.y = GetTerrainHeight(terrain, position.x, position.z) + yOffset;
-
-                Quaternion rotation =
-                    Quaternion.FromToRotation(Vector3.up, GetTerrainNormal(terrainData, position.x, position.z)) *
-                    Quaternion.Euler(0, Random.Range(0, 360), 0);
-
-                GameObject building = buildingPrefabs[Random.Range(0, buildingPrefabs.Length)];
-                Instantiate(building, position, rotation, terrain.transform);
-            }
-        }
-    }
-
-    private float GetTerrainHeight(Terrain terrain, float x, float z) {
-        return terrain.SampleHeight(new Vector3(x, 0, z));
-    }
-
-    private Vector3 GetTerrainNormal(TerrainData terrainData, float x, float z) {
-        return terrainData.GetInterpolatedNormal(x / size, z / size);
     }
 }
