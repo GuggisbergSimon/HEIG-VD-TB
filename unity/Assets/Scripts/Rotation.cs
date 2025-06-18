cbe24918f81a6324cf7e207ccd5023fae46cbba8
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2025-06-18 12:59:59 +0200

    feat: improve text readability and loading screen

---
+++ unity/Assets/Scripts/Rotation.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+    [SerializeField] private Vector3 rotationSpeed = new Vector3(0f, 0f, 50f);
+    [SerializeField] private bool isTimeScaleIndependent = false;
+
+    void Update() {
+        transform.Rotate(rotationSpeed * (isTimeScaleIndependent ? Time.unscaledDeltaTime : Time.deltaTime),
+            Space.Self);
+    }
+}
